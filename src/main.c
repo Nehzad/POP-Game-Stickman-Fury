13e9a7ab91995a8731700aab6b669b96f0933cfd
Nehzad <Nehzad@users.noreply.github.com>
2026-05-26 22:12:12 +0100

    Stabilize green player sprite anchors

--- src/main.c
+++ src/main.c
@@ -91,11 +91,11 @@ int main(void)
                     enemy.velX = -2.2f;
                     enemy.facing = -1;
                 }
-                else if (enemyOffset < -12.0f)
+                else if (enemyOffset < -28.0f)
                 {
                     enemy.facing = 1;
                 }
-                else if (enemyOffset > 12.0f)
+                else if (enemyOffset > 28.0f)
                 {
                     enemy.facing = -1;
                 }
@@ -205,7 +205,8 @@ int main(void)
                             assets.playerDoubleJump,
                             assets.playerPunch,
                             assets.playerKick,
-                            frameCounter);
+                            frameCounter,
+                            false);
 
         DrawAnimatedFighter(enemy,
                             assets.enemyIdle,
@@ -214,7 +215,8 @@ int main(void)
                             assets.enemyDoubleJump,
                             assets.enemyPunch,
                             assets.enemyKick,
-                            frameCounter);
+                            frameCounter,
+                            true);
 
         DrawHitEffects();
 
--- src/rendering.h
+++ src/rendering.h
@@ -13,7 +13,8 @@ void DrawAnimatedFighter(Fighter f,
                          Texture2D doubleJumpTex,
                          Texture2D punchTex,
                          Texture2D kickTex,
-                         int frameCounter);
+                         int frameCounter,
+                         bool stabilizeFrameAnchor);
 void DrawBackgroundScaled(Texture2D bg);
 void DrawGroundAndShadows(Fighter player, Fighter enemy);
 
